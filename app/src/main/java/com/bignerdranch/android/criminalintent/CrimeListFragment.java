d3a5a5dcaafd8a8956d6283b2bb07912c4b57d11
Jeffrey Lee <jlee@spokeo.com>
2016-10-21 09:54:40 -0700

    working on ch 9

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by jlee on 10/20/16.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
